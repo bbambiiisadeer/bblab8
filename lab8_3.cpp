d957ae3ff409038e98e4e21b1456999e1eb70f4f
bbam <nunnapats2940@gmail.com>
2022-12-20 16:18:34 +0700

    lab3_completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(65<=x&&x<=90){
+    if(x=='A'){
+        return 'Z';
+    }else{
+        return x-1;
+    }
+    }
+return '0';
 }
 
 int main(){
